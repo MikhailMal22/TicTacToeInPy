d90be67ee1ade997e23048744b19bc414d75f639
MikhailMal22 <mmalinovskiy24@gmail.com>
2025-05-11 16:19:54 -0400

    Improved computer AI
    Computer player is now not entirely random, and has basic AI.

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ def printBoard():
     print(f" {board[6]} | {board[7]} | {board[8]} ")
     print()
 
-def plrMove():
+def plrMove(available,XList):
     while True:
         spot = input("Where will you place an X (1-9)?")
         if not spot.isdigit():
@@ -25,15 +25,64 @@ def plrMove():
         elif board[int(spot)-1] == PLR or board[int(spot)-1] == COM:
             print("This spot is already taken")
         else:
-            board[int(spot)-1] = PLR
+            spotInt = int(spot)-1
+            board[spotInt] = PLR
+            XList.append(spotInt)
+            available.remove(spotInt)
             break
 
-def comMove():
-    while True:
+def comPlace(list,available,i):
+    board[i] = COM
+    list.append(i)
+    available.remove(i)
+
+def comMove(available,OList,XList):
+
+    if len(available) > 1 and len(available) < 9:
+        candidate = 0
+        if len(XList) > 1:
+            #print("Xlist is bigger than one, looking for a winList")
+            k = XList[-1]
+            j = XList[-2]
+            for list in winList:
+                if k in list and j in list:
+                    candidate = list
+                    #print(f"found potential candidate:{candidate}")
+                    break
+            if candidate != 0:
+                for num in candidate:
+                    if num != k and num != j and num in available:
+                        i = num
+                        comPlace(OList,available,i)
+                        break
+                else:
+                    candidate = 0
+
+        if candidate == 0:
+            #print("either list is size one or candidate wasnt found")
+            j = XList[-1]
+            for list in winList:
+                if j in list:
+                    candidate = list
+                    #print(f"found potential candidate {candidate}")
+                    for num in candidate:
+                        if num != j and num in available:
+                            i = num
+                            comPlace(OList,available,i)
+                            break
+                    else:
+                        continue
+                    break
+
+    elif len(available) == 9:
+        #print("all spots in the board are available")
         i = random.randint(0,8)
-        if board[i] != PLR and board[i] != COM:
-            board[i] = COM
-            break
+        comPlace(OList,available,i)
+
+    elif len(available) == 1:
+        #print("one spot left")
+        i = available[0]
+        comPlace(OList,available,i)
 
 def checkIfWin(s):
     for subList in winList:
@@ -71,29 +120,38 @@ def main():
     game = True
     currentPlr = PLR
     spotsAvailable = 9
+    available = [0,1,2,3,4,5,6,7,8]
+    XList = []
+    OList = []
     while game == True:
         printBoard()
         time.sleep(0.5)
 
         if currentPlr == PLR:
             print("Your Turn!")
-            plrMove()
+            plrMove(available,XList)
             spotsAvailable -= 1
 
         if currentPlr == COM:
             print("Computer's turn!")
-            comMove()
+            comMove(available,OList,XList)
             spotsAvailable -= 1
 
         if checkIfWin(currentPlr):
             game = playAgain(game)
             spotsAvailable = 9
+            available = [0,1,2,3,4,5,6,7,8]
+            XList = []
+            OList = []
 
         if spotsAvailable <= 0:
             print("Draw")
             printBoard()
             game = playAgain(game)
             spotsAvailable = 9
+            available = [0,1,2,3,4,5,6,7,8]
+            XList = []
+            OList = []
 
         currentPlr = changePlayer(currentPlr)
 
